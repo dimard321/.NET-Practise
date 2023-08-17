8b4aeb57dffde14641028c77918b19942be87de4
MariaGracheva <mariya.gracheva@icloud.com>
2023-08-17 18:48:41 +0300

    Второе дз выполнено

---
+++ HomeWork2/Students/Students/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace Students
+{
+    public class Program
+    {
+        /// <summary>
+        /// Заполняет словарь рандомным предметами для каждого студента
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        static HashSet<string> GetRandomSubjects(string[] array)
+        {
+            var subjects = new HashSet<string>();
+
+            var rnd = new Random();
+
+            while (subjects.Count < 3)
+            {
+                string subject = array[rnd.Next(6)];
+
+                subjects.Add(subject);
+            }
+
+            return subjects;
+        }
+        /// <summary>
+        /// В основном методе создается словарь из студентов и их предметов
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            var subjects = new string[6] { "Maths", "History", "Literature", "Physics", "Biology", "PE" };
+
+            var student1c2 = new Student("Vasya", "Pupkin");
+
+            var student2c2 = new Student("Rima", "Tarakanova");
+
+            var student3c2 = new Student("Kolya", "Bobrov");
+
+            var student1c1 = new Student("vasya.pupkin@mail.ru");
+
+            var student2c1 = new Student("rima.tarakanova@mail.ru");
+
+            var student3c1 = new Student("kolya.bobrov@mail.ru");
+
+            var studentSubjectDict = new Dictionary<Student, HashSet<string>>()
+            {
+
+                [student1c1] = GetRandomSubjects(subjects),
+
+                [student2c1] = GetRandomSubjects(subjects),
+
+                [student3c1] = GetRandomSubjects(subjects),
+
+                [student1c2] = GetRandomSubjects(subjects),
+
+                [student2c2] = GetRandomSubjects(subjects),
+
+                [student3c2] = GetRandomSubjects(subjects)
+
+            };
+
+            foreach (var item in studentSubjectDict)
+            {
+                Console.WriteLine($"Имя студента: {item.Key.FullName}");
+
+                Console.WriteLine("Название предметов:");
+
+                foreach (var item2 in item.Value)
+                {
+                    Console.WriteLine($"{item2}\t");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ HomeWork2/Students/Students/Student.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Students
+{
+    /// <summary>
+    /// Класс Student
+    /// </summary>
+    internal class Student
+    {
+        /// <summary>
+        /// Принимает и отдает полное имя и email
+        /// </summary>
+        public string FullName { get; set; }
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Конструктор класса Student, который принимает email и возвращает полное имя
+        /// </summary>
+        /// <param name="email"></param>
+        public Student(string email)
+        {
+            Email = email;
+
+            FullName = ConvertToFullName(email);
+        }
+
+        /// <summary>
+        /// Конструктор класса Student, который принимает имя и фамилию, а возвращает email и полное имя
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="surname"></param>
+        public Student(string name, string surname) 
+        {
+            FullName = name + " " + surname;
+
+            Email =  ConvertToEmail(name, surname);       
+        }
+
+        /// <summary>
+        /// Переопределенный метод equals сравнивает по почте 
+        /// </summary>
+        /// <param name="obj">объект</param>
+        /// <returns>true - объекты равны, false - не равны</returns>
+        public override bool Equals(object obj)
+        {
+           if (obj == null || GetType() != obj.GetType())
+                return false;
+
+           var s = (Student)obj;
+
+            return Email == s.Email;
+        }
+
+        /// <summary>
+        /// Назначает почте хеш код
+        /// </summary>
+        /// <returns>возвращает хеш код</returns>
+        public override int GetHashCode()
+        {
+            return Email.GetHashCode();
+        }
+
+        /// <summary>
+        /// Извлечение полного имени из email
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns>полное имя</returns>
+        public string ConvertToFullName(string Email)
+        {
+           //извлечение имени из почты
+   
+            char dot = '.';
+
+            char at = '@';
+
+            int indexOfDot = Email.IndexOf(dot);
+
+            string firstName = Email.Substring(0, indexOfDot);
+
+            string tempFirstName = firstName.ToUpper();
+
+            tempFirstName = tempFirstName.Substring(0, 1);
+
+            firstName = firstName.Substring(1);
+
+            firstName = tempFirstName + firstName;
+
+            // извлечение фамилии из почты
+
+            string newEmail = Email.Substring(indexOfDot + 1);
+
+            int indexOfDotTwo = newEmail.IndexOf(dot);
+
+            int indexOfAt = newEmail.IndexOf(at);
+
+            string lastName = Email.Substring(indexOfDot + 1, indexOfAt);
+
+            string tempLastName = lastName.ToUpper();
+
+            tempLastName = tempLastName.Substring(0, 1);
+
+            lastName = lastName.Substring(1);
+
+            lastName = tempLastName + lastName;
+
+            return FullName = firstName + " " + lastName;
+        }
+
+        /// <summary>
+        /// Переобразует полное имя в почту
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="surname"></param>
+        /// <returns>почту</returns>
+        public string ConvertToEmail(string name, string surname)
+        {
+            name = name.ToLower();
+
+            surname = surname.ToLower();
+
+            string email = name.Insert(name.Length, ".");
+
+            email = email.Insert(email.Length, surname);
+           
+            email = email.Insert(email.Length, "@mail.ru");
+
+            return email;
+        }
+    }
+}
