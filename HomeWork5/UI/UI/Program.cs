12bfb4276d2e0fa7aeee92aef59c4e535a051677
MariaGracheva <mariya.gracheva@icloud.com>
2023-09-19 16:14:43 +0300

    Отправляю 5 дз

---
+++ HomeWork5/UI/UI/Program.cs
@@ -0,0 +1,45 @@
+﻿using NLog;
+using StringConverterLibrary;
+
+namespace UI
+{
+    public class Program
+    {
+        private static readonly NLog.ILogger logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+
+            var str = new StringConverter(logger);
+
+            var inputString = "645";
+
+            try
+            {
+                str.Validate(inputString);
+
+                int result = str.ConvertToInt(inputString);
+
+                Console.WriteLine($"Результат: {result}");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                return;
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                logger.Error($"Необработанное исключение: {ex.Message}");
+
+                Console.WriteLine($"Необработанное исключение: {ex.Message}");
+
+                return;
+            }
+        }
+    }
+}
